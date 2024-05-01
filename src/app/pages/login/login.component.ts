761186d4de56b2f5504ceb0f993f9c0cedf336a5
JonathanGS02 <jonathangs02@hotmail.com>
2024-05-01 15:48:34 -0300

    Ajuste Login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router'; // Importe o serviço Router
 
 @Component({
   selector: 'app-login',
@@ -15,19 +16,49 @@ export class LoginComponent {
   confirmPassword: string = '';
   username: string = ''; 
 
+  constructor(private router: Router) { } // Injete o serviço Router
+
   toggleForm(form: 'login' | 'register' | 'forgot') {
     this.currentForm = form;
+    // Limpar os campos quando o formulário for alterado
+    this.clearFields();
   }
 
   togglePassword() {
     this.passwordHidden = !this.passwordHidden;
   }
 
-  submitForm(registerForm: any) {
-    if (registerForm.valid) {
+  submitForm(formType: 'login' | 'register' | 'forgot', form: any) {
+    if (form.valid) {
       console.log('Formulário válido. Enviando...');
-    } else {
-      console.log('Formulário inválido. Corrija os campos marcados.');
-    }
+
+      if (formType === 'register') {
+        // Navegar para a tela de login após enviar o formulário de cadastro com sucesso
+        this.currentForm = 'login';
+        this.clearFields();
+      } 
+
+      if (formType === 'login') {
+        // Lidar com o envio do formulário de login
+        this.router.navigate(['/home']);
+        this.clearFields();
+      }
+
+      if(formType === 'forgot') {
+        this.currentForm = 'login';
+        this.clearFields();
+      }
+    } 
+  }
+  
+
+  clearFields() {
+    // Limpar os campos do formulário
+    this.fullname = '';
+    this.email = '';
+    this.confirmEmail = '';
+    this.password = '';
+    this.confirmPassword = '';
+    this.username = '';
   }
 }
